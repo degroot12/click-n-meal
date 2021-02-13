0a8d3d7e8031f06e8955610d8092e73327c5392b
Cindy <cindytvn+git@gmail.com>
2021-02-13 17:04:41 +0100

    finished seeding data ingredients

---
+++ bin/ingredients.seeds.js
@@ -0,0 +1,25 @@
+// seeding the ingredients from World Open Foodfacts: https://wiki.openfoodfacts.org/API
+
+require('../db/index')
+
+const IngredientsModel = require('../models/Ingredients.model')
+const mongoose = require('mongoose')
+const axios = require('axios')
+
+axios.get('https://world.openfoodfacts.org/ingredients.json')
+  .then((response) => {
+    IngredientsModel.insertMany(response.data.tags)
+    // console.log(response.data.tags)
+      .then(() => {
+        console.log('data ingredients inserted')
+        mongoose.connection.close()
+      })
+      .catch((err) => {
+        console.log('Error while retrieving data: ', err)    
+        mongoose.connection.close()   
+      })
+  })
+  .catch((err) => {
+    console.log('Error while retrieving data: ', err)
+  });
+
---
+++ models/Ingredients.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const IngredientsSchema = new mongoose.Schema({
+  name: String
+});
+
+
+const IngredientsModel = mongoose.model('ingredient', IngredientsSchema);
+
+module.exports = IngredientsModel;
