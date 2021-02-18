de32495a1f296c979caaff92992aa040c877961d
Cindy <cindytvn+git@gmail.com>
2021-02-18 09:30:40 +0100

    default picture of empty plate

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -4,7 +4,10 @@ const IngredientsModel = require('./Ingredients.model.js')
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const RecipeSchema = new mongoose.Schema({
   name: String,
-  image: String,
+  image: {
+    type: String,
+    default: "https://images.unsplash.com/photo-1525999147711-835474620964?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cGxhdGV8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  },
   description: String,
   instructions: String,
   time: Number,
